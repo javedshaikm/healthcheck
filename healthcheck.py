7a71756be45c381915366beb987d7a1904124b86
javedshaikm <javed.s.m@hotmail.com>
2018-10-08 11:09:53 +0530

    Update healthcheck.py
    Fully working script which can login into cisco devices, read interface and bgp information and send mail on the status.

--- healthcheck.py
+++ healthcheck.py
@@ -12,12 +12,10 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
 username = 'cisco'
-password = 'cisco_1234!'
-hosts = ['10.10.20.48']
-platform = 'cisco_xe'
-
+password = 'cisco'
+hosts = ['10.0.10.100','10.0.10.101']
+platform = 'cisco_ios'
 
 def send_mail():
 	fromaddr = "@gmail.com"
@@ -29,12 +27,12 @@ def send_mail():
 	msg.attach(MIMEText(status, 'plain'))	
 	server = smtplib.SMTP('smtp.gmail.com', 587)
 	server.starttls()
-	server.login(fromaddr, "")
+	server.login(fromaddr, "password")
 	text = msg.as_string()
 	server.sendmail(fromaddr, toaddr, text)
 	server.quit()
 
-
+status = " "
 for host in hosts:
 	connect = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
 	output = connect.send_command('terminal length 0', expect_string=r'#')
@@ -45,24 +43,23 @@ for host in hosts:
 	interface_result = StringIO()
 	sys.stdout = interface_result
 	sys.stdout = old_stdout
-	data = pd.read_fwf(StringIO(interface_status),  widths=[23, 16, 3, 7, 22, 8])
-	status = " "
+	data = pd.read_fwf(StringIO(interface_status),  widths=[27, 16, 3, 7, 23, 8])
 	for index, row in data.iterrows():
 		if row[4] == 'administratively down' or row[4] == 'down':
 			log = (f"\nInterface {row[0]} is down in {host_name}\n")
 			status += log
 	
-
 	bgp_status = connect.send_command('show ip bgp summary | be N',expect_string=r'#')
 	old_stdout = sys.stdout
 	bgp_result = StringIO()
 	sys.stdout = bgp_result
 	sys.stdout = old_stdout
 	bgp_data = pd.read_fwf(StringIO(bgp_status),  delim_whitespace=True, header=None)
+	
 	for index, row in bgp_data.iterrows():
 		if row[9] == 'Down' or row[9] == 'Idle' or row[9] == 'Active':
 			bgp = (f"\nNeighbor {row[0]} is down in {host_name}\n")
 			status += bgp
-	print(status)
-	send_mail()
+send_mail()
+	
 	
